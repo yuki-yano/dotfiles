e39c0d06c0bc4ba589613cb8fa2ec8e12f72645f
yuki_yano <yuki.ycino@gmail.com>
2018-02-08 21:21:57 +0900

    [vimperator] 不要なvimperatorプラグインを削除

--- .vimperator/local-plugins/open-blisk.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    commands.addUserCommand(
-      ['blisk', 'bl'],
-      'Open the buffer URL in blisk',
-      function(args){
-        io.system('open -a "/Applications/Blisk.app" "' + buffer.URL + '"');
-      }
-    );
-})();
