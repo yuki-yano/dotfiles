77b1a3f4e6338ec5c3076403f606edd5f7c342f8
yuki_yano <yuki.ycino@gmail.com>
2021-09-20 12:54:39 +0900

    [vimperator] vimperatorの設定を削除

--- .vimperator/local-plugins/open-chrome.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    commands.addUserCommand(
-      ['chrome', 'ch'],
-      'Open the buffer URL in chrome',
-      function(args){
-        io.system('open -a "/Applications/Google\ Chrome.app" "' + buffer.URL + '"');
-      }
-    );
-})();
